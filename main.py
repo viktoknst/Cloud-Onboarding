af7fd9d69363d537d992c5dd50f988d182d40df5
Alex_laptop <aleksandriliev05@gmail.com>
2024-07-09 11:02:11 +0300

    Added unit test

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+print("Hello world")
 
-
-print("helo world!")
+def add_two_nums(a: int, b: int):
+    if isinstance(a, (int, float)) and isinstance(b, (int,float)):
+        return a+b
+    raise Exception
---
+++ test_main.py
@@ -0,0 +1,13 @@
+import pytest
+import main
+
+def test_add_two_nums():
+    assert main.add_two_nums(1,2) == 3
+    assert main.add_two_nums(6,-8.5) == -2.5
+    with pytest.raises(Exception) as ex: # assert an exception is raised
+        assert main.add_two_nums(1,2) == 4
+    with pytest.raises(Exception) as ex:
+        main.add_two_nums("Helo", None)
+
+if __name__ == '__main__':
+    test_add_two_nums()
