f1fd526f10c87cabda51fe3bf67631095b79f842
Alex_laptop <aleksandriliev05@gmail.com>
2024-07-16 14:45:03 +0300

    Updated result crud

--- src/app/crud/result.py
+++ src/app/crud/result.py
@@ -5,14 +5,18 @@ from app.models.result import Result
 def create(db: Database, id: str):
     db['results'].insert_one(
         {
-        'uuid': id,
+        'id': id,
         'status': 'running'
         }
     )
 
 
-def read():
-    pass
+def read(db: Database, id: str) -> Result:
+    return db['results'].find_one(
+        {
+            'id': id
+        }
+    )
 
 
 def update(db: Database, r: Result):
@@ -29,5 +33,9 @@ def update(db: Database, r: Result):
     )
 
 
-def delete():
-    pass
+def delete(db: Database, id: str):
+    db['results'].delete_one(
+        {
+            'id': id
+        }
+    )
