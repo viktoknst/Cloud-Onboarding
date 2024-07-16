de46d255b77819cc2a04b407fff64b1c35a18337
Alex_laptop <aleksandriliev05@gmail.com>
2024-07-16 12:55:53 +0300

    Partial

--- src/app/routers/user.py
+++ src/app/routers/user.py
@@ -2,39 +2,27 @@ from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 
 from src.config import ENDPOINTS
-from src.config import USER_DB
 
+from app.schemas.user import *
 
 user_router = APIRouter()
 
-class UserCreate(BaseModel):
-    user_name: str
-
-class User(BaseModel):
-    user_name: str
-
 
 @user_router.post(ENDPOINTS['create_user'])
-def post_create_user(u: UserCreationRequest):
-    if re.match(r'^[a-zA-Z0-9_-]{4,16}$', u.user_name) == None:
-        raise HTTPException(409, "Username is invalid")
-
-    if os.path.exists(USER_DB['user_dir']+"/"+u.user_name):
-        raise HTTPException(409, "Username is in use!")
-
-    os.mkdir(USER_DB['user_dir']+"/"+u.user_name)
-    return {'msg': 'User created'}
-
-
-@smt_router.post()
-def rsrc_create():
+def create_user(u: UserCreate):
     pass
-@smt_router.get()
-def rsrc_read():
+
+
+@user_router.get()
+def read_user():
     pass
-@smt_router.put()
-def rsrc_update():
+
+
+@user_router.put()
+def update_user():
     pass
-@smt_router.delete()
-def rsrc_delete():
+
+
+@user_router.delete()
+def delete_user():
     pass
--- src/app/schemas/user.py
+++ src/app/schemas/user.py
@@ -1,1 +1,9 @@
 from pydantic import BaseModel
+
+
+class UserCreate(BaseModel):
+    user_name: str
+
+
+class User(BaseModel):
+    user_name: str
