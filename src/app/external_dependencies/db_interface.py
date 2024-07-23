9e693c0564086a1e1d9e15df20085251905ec987
Alex_laptop <aleksandriliev05@gmail.com>
2024-07-23 15:48:23 +0300

    Changed import order in db_interface

--- src/app/external_dependencies/db_interface.py
+++ src/app/external_dependencies/db_interface.py
@@ -2,8 +2,8 @@ from pymongo import MongoClient
 
 CONNECTION_STRING = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.10"
 
-# singleton which holds the db; unnescessary as mongoclient is threadsafe but thats not stopping me
 class DBProxy:
+    # singleton which holds the db; unnescessary as mongoclient is threadsafe but thats not stopping me
     __instance = None
 
     def __init__(self):
@@ -12,7 +12,7 @@ class DBProxy:
 
     @classmethod
     def get_instance(cls):
-        if cls.__instance == None:
+        if cls.__instance is None:
             cls.__instance = DBProxy()
         return cls.__instance
 
