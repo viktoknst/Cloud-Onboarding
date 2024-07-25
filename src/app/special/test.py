a2d0aed7e06dc1fab8ad465962bfdf379848f799
Alex_laptop <aleksandriliev05@gmail.com>
2024-07-25 15:39:16 +0300

    Minor text changes

--- src/app/special/test.py
+++ src/app/special/test.py
@@ -1,7 +1,11 @@
-print("Hello world")
-
+'''
+File to confirm tests are working
+'''
 
 def add_two_nums(a: int, b: int):
+    '''
+    s.e.
+    '''
     if isinstance(a, (int, float)) and isinstance(b, (int,float)):
         return a+b
     raise Exception
