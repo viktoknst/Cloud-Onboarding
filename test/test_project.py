1bbe3c77d722dd5901772505487487bf3af0451e
Alex_laptop <aleksandriliev05@gmail.com>
2024-07-26 15:06:55 +0300

    Add test create project

--- test/test_project.py
+++ test/test_project.py
@@ -2,14 +2,21 @@ import context
 from context import client
 from unittest import mock
 
+def func(*args):
+    '''
+    Does nothing. Use for tests.
+    '''
+
 class TestProject():
-    @mock.patch('app.external_dependencies.db_interface.DBProxy', new=context.MockDBProxy)
+    @mock.patch('os.mkdir', new=func)
+    @mock.patch('app.external_dependencies.db_interface.DBProxy.get_instance', new=context.MockDBProxy.get_instance)
     def test_create(self):
         responce = client.post('/project', json={'user_id':'abc', 'project_name':'project'})
-        assert responce.status_code == 409
-        context.MockDBProxy.get_instance().get_db()['users'].insert_one({'id':'abc', 'dir':'~/testing'})
+        assert responce.status_code == 409 # User abd doesnt exist
+        # create user abc
+        context.MockDBProxy.get_instance().get_db()['users'].insert_one({'id':'abc', 'dir':''})
         responce = client.post('/project', json={'user_id':'abc', 'project_name':'project'})
-        assert responce.status_code == 200
+        assert responce.status_code == 200 # Now works
 
     def test_update(self):
         pass
