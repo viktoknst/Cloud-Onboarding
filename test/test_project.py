cef8f13720724419edf65ba5385f24b77d07634a
Alex_laptop <aleksandriliev05@gmail.com>
2024-07-26 16:54:14 +0300

    Formating

--- test/test_project.py
+++ test/test_project.py
@@ -8,11 +8,18 @@ def func(*args):
     '''
 
 class TestProject():
+    '''
+    Tests project-related endpoints
+    '''
     @mock.patch('os.mkdir', new=func)
     @mock.patch('app.external_dependencies.db_interface.DBProxy.get_instance', new=context.MockDBProxy.get_instance)
-    def test_create(self):
+    def test_create_fail(self):
         responce = client.post('/project', json={'user_id':'abc', 'project_name':'project'})
         assert responce.status_code == 409 # User abd doesnt exist
+
+    @mock.patch('os.mkdir', new=func)
+    @mock.patch('app.external_dependencies.db_interface.DBProxy.get_instance', new=context.MockDBProxy.get_instance)
+    def test_create_work(self):
         # create user abc
         context.MockDBProxy.get_instance().get_db()['users'].insert_one({'id':'abc', 'dir':''})
         responce = client.post('/project', json={'user_id':'abc', 'project_name':'project'})
